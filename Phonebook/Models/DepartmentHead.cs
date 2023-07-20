e6b2f23cda7263f79543adefd052353ae8939541
Andrey Borisevich <zilens2lan@gmail.com>
2023-07-20 17:36:49 +0300

    refactoring models

--- Phonebook/Models/DepartmentHead.cs
+++ Phonebook/Models/DepartmentHead.cs
@@ -1,10 +1,17 @@
-﻿namespace Phonebook.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phonebook.Models
 {
     public class DepartmentHead
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Phone { get; set; }
+        public List<Worker> Workers { get; set; }
+        public Director Director { get; set; }
     }
 }
--- Phonebook/Models/Director.cs
+++ Phonebook/Models/Director.cs
@@ -1,10 +1,16 @@
-﻿namespace Phonebook.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phonebook.Models
 {
     public class Director
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Phone { get; set; }
+        public List<DepartmentHead> DepartmentsHeads { get; set; }
     }
 }
